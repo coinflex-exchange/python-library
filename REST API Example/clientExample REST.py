0eed5710b27fc34ea6e7d34c16e252367fdcb146
coinflexdarran <52005264+coinflexdarran@users.noreply.github.com>
2020-01-24 18:09:16 +0000

    Update clientExample REST.py

--- REST API Example/clientExample REST.py
+++ REST API Example/clientExample REST.py
@@ -1,7 +1,6 @@
 import requests
 import base64
 import time
-from config import args_stage
 
 class args:
     url = "https://webapi.coinflex.com"  # change URL between STAGE and LIVE
@@ -10,31 +9,32 @@ class args:
     phrase = ''  # this is password for your CoinFLEX account found in API Details
 
 # Authentication
-auth = str(args_stage.id) + "/" + args_stage.cookie
-string = auth + ':' + args_stage.phrase
+auth = str(args.id) + "/" + args.cookie
+string = auth + ':' + args.phrase
 bytes = base64.b64encode(string.encode())
 auth = bytes.decode()
 headers = {'authorization': 'Basic ' + auth, 'Content-type': 'application/x-www-form-urlencoded'}
 
 # Example REST API calls
-response = requests.get(args_stage.rest_url + "orders/", headers=headers)
+# GET current open orders
+response = requests.get(args.url + "/orders/", headers=headers)
 print(response.json())
 
-#response = requests.delete(args_stage.rest_url + "orders/", headers=headers)
-#print(response.json())
+# Cancel ALL current open orders
+response = requests.delete(args.url + "/orders/", headers=headers)
+print(response.json())
 
-#response = requests.get(args_stage.rest_url + "orders/", headers=headers)
-#print(response.json())
+# GET markets reference information
+response = requests.get(args.url + "/markets/", headers=headers)
+print(response.json())
 
-#response = requests.get(args.url + "/markets/", headers=headers)
-#print(response.json())
-
-#response = requests.get(args.url + "/tickers/", headers=headers)
-#print(response.json())
+# GET tickers 
+response = requests.get(args.url + "/tickers/", headers=headers)
+print(response.json())
 
 # Example REST API calls where authentication is required
-#response = requests.get(args.url + "/balances/", headers=headers)
-#print(response.json())
+response = requests.get(args.url + "/balances/", headers=headers)
+print(response.json())
 
 # Authenticated GET API call with input parameters
 previous_24h = (time.time() - 24 * 60 * 60) * 1000000
